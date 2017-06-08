72b28d51f028238f2f58810e88372cebfe80abb9
Ralph Dugue <rdugue1@gmail.com>
2017-06-08 02:30:07 -0400

    Refactored database operations
    -moved database operations to a helper module
    -created handler function stubs for API CRUD events
    -updated cloudformation template

--- auth_login.py
+++ auth_login.py
@@ -1,42 +1,21 @@
 from __future__ import print_function
 import json
-import boto3
 import jwt
 from passlib.hash import pbkdf2_sha256
-from botocore.exceptions import ClientError
+from shifty_utils import respond, get_user
 
 print('Loading function')
-dynamo = boto3.client('dynamodb')
 JWT_SECRET = 'secret'
 JWT_ALGORITHM = 'HS256'
 
-
-def respond(err, res=None):
-    return {
-        'statusCode': '400' if err else '200',
-        'body': err['message'] if err else res,
-        'headers': {
-            'Content-Type': 'application/json',
-        },
-    }
 
 def lambda_handler(event, context):
     print("Received login attempt: " + json.dumps(event, indent=2))
 
     payload = event['body']
 
-    try:
-        response = dynamo.get_item(
-            TableName=payload['company'] + '_users',
-            Key={
-                'userId': payload['userId'],
-                'position': payload['posittion']
-            }
-        )
-    except ClientError as e:
-        print(e.response['Error']['Message'])
-        return respond({'message': e.response['Error']['Message']})
-    else:
+    response = get_user(payload)
+    if 'Item' in response:
         user = response['Item']
         if pbkdf2_sha256.verify(payload['password'], user['password']):
             jwt_payload = {
@@ -51,4 +30,5 @@ def lambda_handler(event, context):
         else:
             print("Credentials could not be verified:" + json.dumps(user, indent=2))
             return respond({'message': 'Invalid credentials'})
-                
+    else:
+        return respond(response)
